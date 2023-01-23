f2aec16852619af7252cce3db6361a1ca181db29
Andrey <Sm1le84@mail.ru>
2023-01-23 13:21:19 +0300

    initial commit

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,66 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CardDeliveryTest {
+    @BeforeEach
+    void setupTest() {
+
+        open("http://localhost:9999");
+    }
+
+    public static String getFormatDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldTestFormFirst() {
+
+        $("[data-test-id='city'] input").setValue(DataGenerator.generateCity());
+        String date = getFormatDate(DataGenerator.generateDate());
+        SelenideElement data = $("[data-test-id='date'] input");
+        data.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        data.click();
+        data.setValue(date);
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName());
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $("[data-test-id='success-notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + date), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestFormFirstAgain() {
+
+        $("[data-test-id='city'] input").setValue(DataGenerator.generateCity());
+        LocalDate initDate = DataGenerator.generateDate();
+        String date = getFormatDate(initDate);
+        String date2 = getFormatDate(initDate.plusDays(3));
+        SelenideElement data = $("[data-test-id='date'] input");
+        data.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        data.click();
+        data.setValue(date);
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName());
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $("[data-test-id='success-notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + date), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        $("[data-test-id='success-notification'] button").click();
+        data.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        data.click();
+        data.setValue(date2);
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='replan-notification'] .notification__content").shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + date2), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
+}
---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,32 @@
+import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static Faker getFaker() {
+        return new Faker(new Locale("ru"));
+    }
+
+    public static LocalDate generateDate() {
+        long minDay = LocalDate.now().plusDays(3).toEpochDay();
+        long maxDay = LocalDate.now().plusMonths(3).toEpochDay();
+        long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
+        return LocalDate.ofEpochDay(randomDay);
+    }
+
+    public static String generateCity() {
+        return DataGenerator.getFaker().address().city();
+    }
+
+    public static String generateName() {
+        return DataGenerator.getFaker().name().fullName();
+    }
+
+    public static String generatePhone() {
+        return DataGenerator.getFaker().phoneNumber().phoneNumber();
+    }
+}
